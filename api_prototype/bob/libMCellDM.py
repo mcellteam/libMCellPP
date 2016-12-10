00a055d78c2d180664f35ba086cbc5dd5729fdd4
Bob Kuczewski <bobkuczewski@salk.edu>
2016-12-09 21:29:07 -0800

    Began converting some of the data model objects into specific classes.
    When the data model is read, unknown items are created as type
    DataModelItem. Items that are recognized (like mcell.model_objects) will
    be created as their specific type but containing all of the keys found
    in the data model. This allows dynamically created objects to become
    specific classes with specific functionality.
    
    This version has only implemented specific classes for:
    
      Species
      ModelObjects
      GeometryObjects
    
    All other data model items that are recognized by their data model path
    (such as "mcell.define_reactions") are assigned to the "DataModelStub"
    class. Any that are not recognized by their data model path remain as
    DataModelItem.

--- api_prototype/bob/libMCellDM.py
+++ api_prototype/bob/libMCellDM.py
@@ -2,26 +2,29 @@
 
 import json
 
-class DataModelObject(dict):
+
+class DataModelItem(dict):
 
     def __init__(self, dm, path):
         # print ( "Path = " + str(path) )
-        known_paths = ['mcell.viz_output',
-                       'mcell.initialization',
-                       'mcell.simulation_control',
-                       'mcell.geometrical_objects',
-                       'mcell.modify_surface_regions',
-                       'mcell.define_molecules',
-                       'mcell.materials',
-                       'mcell.define_reactions',
-                       'mcell.release_sites',
-                       'mcell.define_surface_classes',
-                       'mcell.reaction_data_output',
-                       'mcell.parameter_system',
-                       'mcell.define_release_sites',
-                       'mcell.scripting',
-                       'mcell.model_objects']
-        if (len(path) > 0) and (path in known_paths):
+        known_paths = {  'mcell.parameter_system' : DataModelStub,
+                         'mcell.initialization' : DataModelStub,
+                         'mcell.geometrical_objects' : GeometryObjects,
+                         'mcell.model_objects' : ModelObjects,
+                         'mcell.define_surface_classes' : DataModelStub,
+                         'mcell.modify_surface_regions' : DataModelStub,
+                         'mcell.define_molecules' : Species,
+                         'mcell.define_reactions' : DataModelStub,
+                         'mcell.release_sites' : DataModelStub,
+                         'mcell.define_release_patterns' : DataModelStub,
+                         'mcell.materials' : DataModelStub,
+                         'mcell.mol_viz' : DataModelStub,
+                         'mcell.viz_output' : DataModelStub,
+                         'mcell.reaction_data_output' : DataModelStub,
+                         'mcell.scripting' : DataModelStub,
+                         'mcell.simulation_control' : DataModelStub  }
+
+        if (len(path) > 0) and (path in known_paths.keys()):
             print ( "Data Model Category = " + path )
 
         if type(dm) == type({}):
@@ -29,9 +32,12 @@ class DataModelObject(dict):
                 v = dm[k]
                 subpath = k
                 if len(path) > 0:
-                  sub_path = path + "." + k
+                    subpath = path + "." + k
                 if type(v) == type({}):
-                    self[k] = DataModelObject(v, subpath)
+                    if (len(subpath) > 0) and (subpath in known_paths.keys()):
+                        self[k] = known_paths[subpath](v, subpath)
+                    else:
+                        self[k] = DataModelItem(v, subpath)
                 elif type(v) == type([]):
                     sub_list = []
                     self.fill_list(sub_list,v, subpath)
@@ -47,7 +53,7 @@ class DataModelObject(dict):
             index = 0
             for item in dm:
                 if type(item) == type({}):
-                    l.append ( DataModelObject(item, path+'['+str(index)+']') )
+                    l.append ( DataModelItem(item, path+'['+str(index)+']') )
                 elif type(item) == type([]):
                     sub_list = []
                     self.fill_list(sub_list,item, path+'['+str(index)+']')
@@ -73,9 +79,26 @@ class DataModelObject(dict):
         else:
             raise AttributeError("No such attribute: " + name)
 
-class Species(DataModelObject):
-    def __init__ ( self, name, dc ):
-        self.dc = dc
+
+class DataModelStub(DataModelItem):
+    pass
+
+
+class Species(DataModelItem):
+    def __init__ ( self, dm, path ):
+        super().__init__(dm,path)
+
+
+class ModelObjects(DataModelItem):
+    def __init__ ( self, dm, path ):
+        super().__init__(dm,path)
+
+
+class GeometryObjects(DataModelItem):
+    def __init__ ( self, dm, path ):
+        super().__init__(dm,path)
+
+
 
 
 dm_str = '{"iters":100, "init": {"warn":"off", "errlim":5 }, "mols": [ {"name":"a", "dc":1e-6 }, {"name":"b", "dc":2e-6 } ] }'
@@ -87,46 +110,52 @@ dm = json.loads ( dm_str )
 
 # print ( str(dm) )
 
-dm = DataModelObject(dm,"")
+dm = DataModelItem(dm,"")
+
+mcell = dm.mcell
 
 print()
-print ( "Source ID   = " + dm.mcell.cellblender_source_sha1 )
-print ( "DM Version  = " + dm.mcell.data_model_version )
-print ( "Iterations  = " + str(dm.mcell.initialization.iterations) )
+for k in mcell.keys():
+  print ( "mcell." + k + " is of type " + str(type(mcell[k])) )
+
 print()
-p = dm.mcell.initialization.partitions
+print ( "Source ID   = " + mcell.cellblender_source_sha1 )
+print ( "DM Version  = " + mcell.data_model_version )
+print ( "Iterations  = " + str(mcell.initialization.iterations) )
+print()
+p = mcell.initialization.partitions
 print ( "Partitions:   x: %s to %s by %s,   y: %s to %s by %s,   z: %s to %s by %s" % (p.x_start, p.x_end, p.x_step, p.y_start, p.y_end, p.y_step, p.z_start, p.z_end, p.z_step, ) )
 print()
-ns = dm.mcell.initialization.notifications
+ns = mcell.initialization.notifications
 print ( "Notifications:" )
 for n in ns.keys():
     print ( "  " + n + ": " + str(ns[n]) )
 print()
-ws = dm.mcell.initialization.warnings
+ws = mcell.initialization.warnings
 print ( "Warnings:" )
 for w in ws.keys():
     print ( "  " + w + ": " + str(ws[w]) )
 print()
 print ( "Parameters:" )
-for p in dm.mcell.parameter_system.model_parameters:
+for p in mcell.parameter_system.model_parameters:
     print ( "  " + p.par_name + " = " + str(p.par_expression) )
 print()
 print ( "Molecules:" )
-for m in dm.mcell.define_molecules.molecule_list:
+for m in mcell.define_molecules.molecule_list:
     print ( "  " + m.mol_name + " is " + m.mol_type + " with diffusion_constant = " + str(m.diffusion_constant) )
 print()
 print ( "Objects:" )
-for o in dm.mcell.geometrical_objects.object_list:
+for o in mcell.geometrical_objects.object_list:
     print ( "  " + o.name + " has " + str(len(o.vertex_list)) + " points and " + str(len(o.element_connections)) + " faces" )
     if "define_surface_regions" in o.keys():
         print ( "    " + o.name + " contains " + str(len(o.define_surface_regions)) + " regions" )
 print()
 print ( "Surface Classes:" )
-for s in dm.mcell.define_surface_classes.surface_class_list:
+for s in mcell.define_surface_classes.surface_class_list:
     print ( "  " + s.name + " has " + str(len(s.surface_class_prop_list)) + " property definitions" )
 print()
 print ( "Modify Surface Regions:" )
-for s in dm.mcell.modify_surface_regions.modify_surface_regions_list:
+for s in mcell.modify_surface_regions.modify_surface_regions_list:
     print ( "  " + s.name )
 print()
 

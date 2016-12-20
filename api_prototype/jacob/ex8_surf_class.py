8a71fbdd9cd3e5c3ae02f8e8446c12a37aa88586
Jacob Czech <czech.jacob@gmail.com>
2016-12-20 16:07:02 -0500

    Update arguments when calling create_box in 5 & 8.
    Use new width argument.

--- api_prototype/jacob/ex8_surf_class.py
+++ api_prototype/jacob/ex8_surf_class.py
@@ -14,7 +14,7 @@ def main():
     transp_a = m.SurfaceProperty("transp_a", m.SP.transp, a_down)
     absorb_a = m.SurfaceProperty("absorb_a", m.SP.absorb, a_down)
     clamp_a = m.SurfaceProperty("clamp_a", m.SP.clamp, a_up, clamp_val=1e-8)
-    box = m.create_box("box")
+    box = m.create_box("box", 1.0)
     box.add_surface_property(transp_a, box.regions["left"])
     box.add_surface_property(absorb_a, box.regions["right"])
     box.add_surface_property(clamp_a, box.regions["top"])

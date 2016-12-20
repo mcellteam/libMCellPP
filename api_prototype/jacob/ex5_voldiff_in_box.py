aaa1187d27863f62764cde727781cbde91266dbd
Jacob Czech <czech.jacob@gmail.com>
2016-12-20 15:45:27 -0500

    Add example 5.

---
+++ api_prototype/jacob/ex5_voldiff_in_box.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+#############################
+# Volumetric diffusion in box
+#############################
+
+import pymcell as m
+
+
+def main():
+    a = m.Species("a", dc=1e-6)
+    box = m.create_box("box")
+    sim = m.Simulation(dt=1e-6)
+    sim.create_molecules_obj(a, box, 100)
+    sim.run_iterations(100)
+
+if __name__ == "__main__":
+    main()

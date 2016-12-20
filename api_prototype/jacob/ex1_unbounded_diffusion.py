39a3a5f03abfa83ba6ef005bb79a34d04f6994ee
Jacob Czech <czech.jacob@gmail.com>
2016-12-20 11:22:06 -0500

    Minor cleanup.

--- api_prototype/jacob/ex1_unbounded_diffusion.py
+++ api_prototype/jacob/ex1_unbounded_diffusion.py
@@ -4,12 +4,12 @@
 # Unbounded diffusion
 #####################
 
-import pymcell
+import pymcell as m
 
 
 def main():
-    a = pymcell.Species("a", dc=1e-6)
-    sim = pymcell.Simulation(dt=1e-6)
+    a = m.Species("a", dc=1e-6)
+    sim = m.Simulation(dt=1e-6)
     sim.create_molecules_shape(a, 100, (0, 0, 0))
     sim.run_iterations(100)
 
--- api_prototype/jacob/ex2_1d_diffusion.py
+++ api_prototype/jacob/ex2_1d_diffusion.py
@@ -4,15 +4,16 @@
 # "1D" diffusion in a thin tube
 ###############################
 
-import pymcell
+import pymcell as m
 import tube
 
+
 def main():
-    a = pymcell.Species("a", dc=1e-6)
-    reg = pymcell.Region("tube_reg", tube.reg_indices)
-    tube_obj = pymcell.MeshObject("tube", tube.verts, tube.faces, [reg])
+    a = m.Species("a", dc=1e-6)
+    reg = m.Region("tube_reg", tube.reg_indices)
+    tube_obj = m.MeshObject("tube", tube.verts, tube.faces, [reg])
     mesh_objs = [tube_obj]
-    sim = pymcell.Simulation(dt=1e-6, meshes=mesh_objs)
+    sim = m.Simulation(dt=1e-6, meshes=mesh_objs)
     sim.create_molecules_reg(a, reg, 100)
     sim.run_iterations(100)
 

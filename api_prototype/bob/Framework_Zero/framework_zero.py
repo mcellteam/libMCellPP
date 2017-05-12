43dc6837d57c453a75e7c20f3d8d6baa147557e4
Bob Kuczewski <bobkuczewski@salk.edu>
2017-05-12 16:55:11 -0700

    Finished up the time-slotted version started before slotted discussion.
    The group held a discussion about whether the events should be stored:
    
      1) In a 1D list sorted by time and other attributes
      2) In a 2D list sorted by time in D1 and other attributes in D2
    
    The discussion concluded with option (1).
    
    The earlier implementations (previous commits) had not yet implemented
    the unique time implementation of option (2), but the work had been
    partially completed in the working directory. This commit contains the
    finished version of option (2) for completeness.

--- api_prototype/bob/Framework_Zero/framework_zero.py
+++ api_prototype/bob/Framework_Zero/framework_zero.py
@@ -27,35 +27,65 @@ class scheduler:
   def __init__ ( self ):
     self.t = 0
     self.slots = {}
-    pass
+
+  def get_slot ( self, t ):
+    slot = None
+    if t in self.slots:
+      slot = self.slots[t]
+    else:
+      slot = self.slots[t] = scheduled_time_slot( t )
+    return slot
 
   def schedule_item_absolute_first ( self, sable_object, t ):
-    pass
+    slot = self.get_slot(t)
+    if slot.absolute_first != None:
+      raise Exception ( "Error, attempt to schedule multiple items first " + str(t) );
+    slot.absolute_first = sable_object
 
   def schedule_item_first ( self, sable_object, t ):
+    slot = self.get_slot(t)
+    slot.obj_list.insert ( 0, sable_object )
     pass
 
   def schedule_item ( self, sable_object, t ):
-    slot = scheduled_time_slot( t ).append ( sable_object )
-    self.slots[t] = slot
-    pass
+    slot = self.get_slot(t)
+    slot.obj_list.append ( sable_object )
 
   def schedule_item_last ( self, sable_object, t ):
+    slot = self.get_slot(t)
+    slot.obj_list.append ( sable_object )
     pass
 
   def schedule_item_absolute_last ( self, sable_object, t ):
-    pass
+    slot = self.get_slot(t)
+    if slot.absolute_last != None:
+      raise Exception ( "Error, attempt to schedule multiple items last at time " + str(t) );
+    slot.absolute_last = sable_object
 
   def run ( self ):
     while len(self.slots) > 0:
       # Get the first slot (earliest time)
       earliest_key = sorted(self.slots.keys())[0]
       slot = self.slots[earliest_key]
-      for o in slot.obj_list:
+      if slot.absolute_first != None:
+        slot.absolute_first.execute ( self, slot.t )
+      while len(slot.obj_list) > 0:
+        o = slot.obj_list[0]
         o.execute ( self, slot.t )
+        slot.obj_list.remove(o)
+      if slot.absolute_last != None:
+        slot.absolute_last.execute ( self, slot.t )
+      self.slots.pop(earliest_key)
 
   def run_through ( self, t ):
     pass
+
+
+class named_schedulable_object(schedulable_object):
+  def __init__(self, name):
+    self.name = name
+  def execute ( self, scheduler, t, data=None):
+    print ( "Executing " + self.name + " at t=" + str(t) )
 
 
 if __name__ == "__main__":
@@ -65,7 +95,22 @@ if __name__ == "__main__":
 
     s.schedule_item ( o, 0 )
     s.schedule_item ( o, 1 )
-    s.schedule_item ( o, 2 )
+    s.schedule_item ( o, 3 )
+    s.schedule_item ( named_schedulable_object('Also'), 3 )
+    s.schedule_item_first ( named_schedulable_object('Before First 3'), 3 )
+    s.schedule_item_last ( named_schedulable_object('After 3'), 3 )
+    s.schedule_item_last ( named_schedulable_object('After After 3'), 3 )
+    s.schedule_item_first ( named_schedulable_object('Before Before First 3'), 3 )
     s.schedule_item ( o, 10 )
+    
+    s.schedule_item ( named_schedulable_object('A20a'), 20 )
+    s.schedule_item ( named_schedulable_object('A20b'), 20 )
+    s.schedule_item ( named_schedulable_object('A21'), 21 )
+    s.schedule_item_absolute_first ( named_schedulable_object('A20_FIRST'), 20 )
+    s.schedule_item_absolute_last  ( named_schedulable_object('A20_LAST'), 20 )
+    s.schedule_item ( named_schedulable_object('A20c'), 20 )
+
+    s.schedule_item ( o, 2 )
+
     s.run()
 

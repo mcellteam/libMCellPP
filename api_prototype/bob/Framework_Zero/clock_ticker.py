11f0c8495f381cfa322a523adfe69b698c3613d7
Bob Kuczewski <bobkuczewski@salk.edu>
2017-05-12 17:11:52 -0700

    Changes during discussion between Tom, Bob, and Oliver
    These changes implemented a callback mechanism rather than the execute
    funcion of the previous version.
    
    This version is most comparable to the versions in commits:
    
      1c200cd24352ddde291bfc506d8952aaef1007ca
      63548fbebec95ed84aa69acea505a23990c8243d

--- api_prototype/bob/Framework_Zero/clock_ticker.py
+++ api_prototype/bob/Framework_Zero/clock_ticker.py
@@ -12,12 +12,20 @@ class clock_ticker(schedulable_object):
         scheduler.schedule_item ( self, t+0.03 )
       scheduler.schedule_item ( self, t+0.1 )
 
+def p ( x ):
+  print ( "Executing my callabck with parameter " + str(x) )
+
 
 if __name__ == "__main__":
 
     s = scheduler()
-    c = clock_ticker()
+    o = schedulable_object()
+    o.callback_list.append ( p )
 
-    s.schedule_item ( c, 0 )
+    s.schedule_item ( o, 0 )
+    s.schedule_item ( o, 1 )
+    s.schedule_item ( o, 2 )
+    s.schedule_item ( o, 10 )
+
     s.run()
 
--- api_prototype/bob/Framework_Zero/framework_zero.py
+++ api_prototype/bob/Framework_Zero/framework_zero.py
@@ -6,20 +6,21 @@ import gtk
 class schedulable_object:
 
   def __init__ ( self ):
+    self.callback_list = []
     pass
 
-  def execute ( self, scheduler, t, data=None ):
-    print ( "Executing at time = " + str(t) )
+  #def execute ( self, scheduler, t, data=None ):
+  #  print ( "Executing at time = " + str(t) )
 
+def p ( x ):
+  print ( "executing with parameter " + str(x) )
 
 class scheduled_time_slot:
-  def __init__ ( self, t ):
+  def __init__ ( self, sable_object, t ):
     self.t = t
     self.absolute_first = None
     self.absolute_last = None
-    self.obj_list = []
-  def append ( self, sable_object ):
-    self.obj_list.append ( sable_object )
+    self.obj = sable_object
     
 
 class scheduler:
@@ -27,90 +28,45 @@ class scheduler:
   def __init__ ( self ):
     self.t = 0
     self.slots = {}
-
-  def get_slot ( self, t ):
-    slot = None
-    if t in self.slots:
-      slot = self.slots[t]
-    else:
-      slot = self.slots[t] = scheduled_time_slot( t )
-    return slot
+    pass
 
   def schedule_item_absolute_first ( self, sable_object, t ):
-    slot = self.get_slot(t)
-    if slot.absolute_first != None:
-      raise Exception ( "Error, attempt to schedule multiple items first " + str(t) );
-    slot.absolute_first = sable_object
+    pass
 
   def schedule_item_first ( self, sable_object, t ):
-    slot = self.get_slot(t)
-    slot.obj_list.insert ( 0, sable_object )
     pass
 
   def schedule_item ( self, sable_object, t ):
-    slot = self.get_slot(t)
-    slot.obj_list.append ( sable_object )
+    slot = scheduled_time_slot( sable_object, t )
+    self.slots[t] = slot
+    pass
 
   def schedule_item_last ( self, sable_object, t ):
-    slot = self.get_slot(t)
-    slot.obj_list.append ( sable_object )
     pass
 
   def schedule_item_absolute_last ( self, sable_object, t ):
-    slot = self.get_slot(t)
-    if slot.absolute_last != None:
-      raise Exception ( "Error, attempt to schedule multiple items last at time " + str(t) );
-    slot.absolute_last = sable_object
+    pass
 
   def run ( self ):
     while len(self.slots) > 0:
-      # Get the first slot (earliest time)
-      earliest_key = sorted(self.slots.keys())[0]
-      slot = self.slots[earliest_key]
-      if slot.absolute_first != None:
-        slot.absolute_first.execute ( self, slot.t )
-      while len(slot.obj_list) > 0:
-        o = slot.obj_list[0]
-        o.execute ( self, slot.t )
-        slot.obj_list.remove(o)
-      if slot.absolute_last != None:
-        slot.absolute_last.execute ( self, slot.t )
-      self.slots.pop(earliest_key)
+      slot = self.slots.pop( sorted(self.slots.keys())[0] )
+      for cb in slot.obj.callback_list:
+        cb(slot.obj)
 
   def run_through ( self, t ):
     pass
 
-
-class named_schedulable_object(schedulable_object):
-  def __init__(self, name):
-    self.name = name
-  def execute ( self, scheduler, t, data=None):
-    print ( "Executing " + self.name + " at t=" + str(t) )
 
 
 if __name__ == "__main__":
 
     s = scheduler()
     o = schedulable_object()
+    o.callback_list.append ( p )
 
     s.schedule_item ( o, 0 )
     s.schedule_item ( o, 1 )
-    s.schedule_item ( o, 3 )
-    s.schedule_item ( named_schedulable_object('Also'), 3 )
-    s.schedule_item_first ( named_schedulable_object('Before First 3'), 3 )
-    s.schedule_item_last ( named_schedulable_object('After 3'), 3 )
-    s.schedule_item_last ( named_schedulable_object('After After 3'), 3 )
-    s.schedule_item_first ( named_schedulable_object('Before Before First 3'), 3 )
-    s.schedule_item ( o, 10 )
-    
-    s.schedule_item ( named_schedulable_object('A20a'), 20 )
-    s.schedule_item ( named_schedulable_object('A20b'), 20 )
-    s.schedule_item ( named_schedulable_object('A21'), 21 )
-    s.schedule_item_absolute_first ( named_schedulable_object('A20_FIRST'), 20 )
-    s.schedule_item_absolute_last  ( named_schedulable_object('A20_LAST'), 20 )
-    s.schedule_item ( named_schedulable_object('A20c'), 20 )
-
     s.schedule_item ( o, 2 )
-
+    s.schedule_item ( o, 10 )
     s.run()
 
